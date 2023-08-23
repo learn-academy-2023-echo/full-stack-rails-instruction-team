179c5da7fa7870e495a0e22ca60524cb7205d718
Sarah Proctor <sproctor950@gmail.com>
2023-08-23 10:33:49 -0700

    added rspec, model and controller for herb, index and show functionality

---
+++ app/controllers/herb_controller.rb
@@ -0,0 +1,10 @@
+class HerbController < ApplicationController
+
+  def index
+    @herbs = Herb.all
+  end
+
+  def show
+    @herb = Herb.find(params[:id]) 
+  end
+end
---
+++ app/helpers/herb_helper.rb
@@ -0,0 +1,2 @@
+module HerbHelper
+end
