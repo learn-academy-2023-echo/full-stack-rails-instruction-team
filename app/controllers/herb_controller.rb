5e6b649d83f86b5ac7b3130411b0a33bc0d3dced
Sarah Proctor <sproctor950@gmail.com>
2023-08-23 11:10:41 -0700

    added new form content and create actions

--- app/controllers/herb_controller.rb
+++ app/controllers/herb_controller.rb
@@ -7,4 +7,22 @@ class HerbController < ApplicationController
   def show
     @herb = Herb.find(params[:id]) 
   end
+
+  def new
+    @herb = Herb.new
+  end
+
+  def create
+    @herb = Herb.create(herb_params)
+    if @herb.valid?
+      redirect_to herbs_path 
+    end
+  end
+
+  # makes all methods below this line ONLY available inside the class - can't be called by the routes
+  private
+  # strong params method
+  def herb_params
+    params.require(:herb).permit(:name, :watered)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'herbs' => 'herb#index', as: 'herbs'
+  get 'herbs/new' => 'herb#new', as: 'new_herb'
   get 'herbs/:id' => 'herb#show', as: 'herb'
+  post 'herbs' => 'herb#create'
   root 'herb#index'
 end
